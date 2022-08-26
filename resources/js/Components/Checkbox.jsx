7df5143db70449386ed83b6dee8fb518d1c38158
Muhammad Rafly Sadewa <raflysdw25@gmail.com>
2022-08-26 08:47:46 +0700

    feat: admin update movies

--- resources/js/Components/Checkbox.jsx
+++ resources/js/Components/Checkbox.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 
-export default function Checkbox({ id, name, value, handleChange, className }) {
+export default function Checkbox({
+    id,
+    name,
+    value,
+    handleChange,
+    checked = false,
+    className,
+}) {
     return (
         <input
             id={id}
@@ -9,6 +16,7 @@ export default function Checkbox({ id, name, value, handleChange, className }) {
             value={value}
             className={`rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 ${className}`}
             onChange={(e) => handleChange(e)}
+            defaultChecked={checked}
         />
     );
 }
